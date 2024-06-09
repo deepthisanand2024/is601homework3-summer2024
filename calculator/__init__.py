499b9852b47269fd081a72c36e795305a8ad4c16
deepthisanand2024 <deepthis.anand@gmail.com>
2024-06-09 14:50:47 -0400

    added all files for Part 1

---
+++ calculator/__init__.py
@@ -0,0 +1,14 @@
+"""Module providing a function printing sum and difference."""
+
+def add(a,b):
+    """Function printing sum of 2 numbers."""
+    return a + b
+def multiply(a,b):
+    """Function printing product of 2 numbers."""
+    return a * b
+def subtract(a,b):
+    """Function printing difference of 2 numbers."""
+    return a - b
+def divide(a,b):
+    """Function printing difference of 2 numbers."""
+    return a / b
